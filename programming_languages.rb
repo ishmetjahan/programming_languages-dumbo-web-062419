7f4365776a911b07c56f80244fe15f02844786bd
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 20:04:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,14 @@ def reformat_languages(languages)
   languages.each do |style, lang|
     lang.each do |name, atr|
       atr.each do |data,val|
-        if newHas.key?(name)==
+        if newHas.key?(name)== false 
+          newHas[name]= {data => value, :style => [type]}
+        else
+          newHas[name][:style] << type
+        end
+      end
+      end
     end
+    newHas
   end
-  newHas
-end
+      
