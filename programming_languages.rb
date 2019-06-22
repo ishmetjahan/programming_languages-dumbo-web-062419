1982a48d57bced717dd009f04e5fd2eed1e762a2
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 19:55:56 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,7 @@
 def reformat_languages(languages)
-  # your code here
+  newHash= {}
+  languages.each do |style, lang|
+    languages.each do |name, atr|
+      newHash[name] ||= atr
+      newHash[name][:style]= []
 end
